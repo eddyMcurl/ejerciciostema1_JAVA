29866c6834cab4edf7ddb3fa52e70bc3b0f1bd25
User <User@DESKTOP-RHBM60S>
2023-01-29 08:56:06 -0500

    listo

---
+++ Entregable1/src/exercice/claseMain.java
@@ -0,0 +1,18 @@
+package exercice;
+
+public class claseMain {
+	public static void main(String[] args) {
+	
+		int entero=5;
+		float variableFloat=2;
+		double variableDouble=2.2;
+		char variableChar='a';
+		String variableString="Cadena";
+		
+		System.out.println("SOY UN ENTERO "+entero);
+		System.out.println("SOY UN FLOAT "+variableFloat);
+		System.out.println("SOY UN DOUBLE "+variableDouble);
+		System.out.println("SOY UN CHAR  "+variableChar);
+		System.out.println("SOY UN STRING "+variableString);
+	}
+}
