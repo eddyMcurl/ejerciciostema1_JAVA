bddb6c6f2bb68772f2988362fd70122f962c90a6
User <User@DESKTOP-RHBM60S>
2023-01-30 18:05:32 -0500

    listo

---
+++ Entregable1/src/exercice/Tema_3.java
@@ -0,0 +1,16 @@
+package exercice;
+
+public class Tema_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] nombreStrings = {"juan","jorge","lucho"};
+		String cadena="";
+		
+		for (String nombreString : nombreStrings) {
+			cadena=cadena+nombreString+" ";
+		}
+		System.out.println(cadena);
+	}
+
+}
