6c4821f3c39f8eff9e265b78acc4ca917d19b3cf
User <User@DESKTOP-RHBM60S>
2023-01-30 18:05:17 -0500

    ok

--- Entregable1/src/exercice/Tema2.java
+++ Entregable1/src/exercice/Tema2.java
@@ -7,13 +7,12 @@ public class Tema2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("ingrese precio");
-		double precio=scanner.nextDouble();
-		Iva(precio);
-	}
-	public static void Iva(double precio) {
-		double iva=precio*0.21;
-		double total=precio+iva;
-		System.out.println("EL PRECIO CON IVA ES: "+ total);
+
+		int edad=16;
+		if(edad>18) {
+			System.out.println("mayor de edad");
+		}else {
+			System.out.println("es menor de edad");
+		}
 	}
 }
